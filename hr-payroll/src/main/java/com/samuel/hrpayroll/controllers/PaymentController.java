4c2779d87791cbaa3e8138f60c3cf02e14ef9c7d
samuel-ferreira-85 <sam85avila@gmail.com>
2023-08-04 20:52:59 -0300

    Inserção do Hystrix - tolerancia a falhas

--- hr-payroll/src/main/java/com/samuel/hrpayroll/controllers/PaymentController.java
+++ hr-payroll/src/main/java/com/samuel/hrpayroll/controllers/PaymentController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.samuel.hrpayroll.entities.Payment;
 import com.samuel.hrpayroll.services.PaymentService;
 
@@ -17,8 +18,13 @@ public class PaymentController {
 	@Autowired
 	private PaymentService paymentService;
 	
+	@HystrixCommand(fallbackMethod = "getPaymentAternative")
 	@GetMapping("/{workerId}/days/{days}")
 	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
 		return ResponseEntity.ok(paymentService.getPayment(workerId, days));
+	}
+
+	public ResponseEntity<Payment> getPaymentAternative(Long workerId, Integer days) {
+		return ResponseEntity.ok(new Payment("Brann", 350.0, days));
 	}
 }
