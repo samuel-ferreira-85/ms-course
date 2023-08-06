11c7cce57bd8f0056469259c815b6fee4ec1e7ff
samuel-ferreira-85 <sam85avila@gmail.com>
2023-08-06 11:00:44 -0300

    Configuração do hr-worker como config-server-client

--- hr-worker/src/main/java/com/samuel/hrworker/controllers/WorkerController.java
+++ hr-worker/src/main/java/com/samuel/hrworker/controllers/WorkerController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,6 +28,15 @@ public class WorkerController {
 
 	@Autowired
 	private IWorkerRepository repository;
+	
+	@Value("${test.config}")
+	private String testConfig;
+	
+	@GetMapping("/configs")
+	public ResponseEntity<Void> getConfigs() {
+		logger.info("CONFIG = " + testConfig);
+		return ResponseEntity.noContent().build();
+	}
 	
 	@GetMapping
 	public ResponseEntity<List<Worker>> findAll() {
