cffb1826b62a5ef433ebb319317a33b08715fd33
samuel-ferreira-85 <sam85avila@gmail.com>
2023-08-13 14:47:21 -0300

    Rodando o hr-worker com profile dev com db postgresql

--- hr-worker/src/main/java/com/samuel/hrworker/HrWorkerApplication.java
+++ hr-worker/src/main/java/com/samuel/hrworker/HrWorkerApplication.java
@@ -3,7 +3,9 @@ package com.samuel.hrworker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories("com.samuel.hrworker.repositories")
 @EnableEurekaClient
 @SpringBootApplication
 public class HrWorkerApplication {
