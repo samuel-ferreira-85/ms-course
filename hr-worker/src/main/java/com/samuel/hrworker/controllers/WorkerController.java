480b78c6c278b95227329d1a8e560a24de0a1508
Samuel Ferreira <93062021+samuel-ferreira-85@users.noreply.github.com>
2023-08-13 13:14:35 -0300

    Revert "Configuração de DB no servidor de configurações"

--- hr-worker/src/main/java/com/samuel/hrworker/controllers/WorkerController.java
+++ hr-worker/src/main/java/com/samuel/hrworker/controllers/WorkerController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
@@ -30,9 +31,12 @@ public class WorkerController {
 	@Autowired
 	private IWorkerRepository repository;
 	
+	@Value("${test.config}")
+	private String testConfig;
+	
 	@GetMapping("/configs")
 	public ResponseEntity<Void> getConfigs() {
-//		logger.info("CONFIG = " + testConfig);
+		logger.info("CONFIG = " + testConfig);
 		return ResponseEntity.noContent().build();
 	}
 	
